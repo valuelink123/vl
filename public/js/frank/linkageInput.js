1a0efde9967594f189c944aa1f6b56c6dbf570f9
Tim <timteam@qq.com>
2018-10-15 09:19:45 +0800

    Signed-off-by: Tim <timteam@qq.com>

--- public/js/frank/linkageInput.js
+++ public/js/frank/linkageInput.js
@@ -38,34 +38,34 @@ class LinkageInput {
         eleInput.setAttribute('list', dataListId)
     }
 
-    static initLinkage(eleInputs, data) {
+    static initLinkage(eleInputs, treeData) {
 
 
         function getListByLayer(targetLayer) {
 
             let layerValueSet = new Set() // 使用集合来去重
 
-            function walk(obj, layer = 0) {
+            function walk(ref, layer = 0) {
 
                 if (0 === layer - targetLayer) {
 
-                    let values = (obj instanceof Array) ? obj : Object.keys(obj)
+                    let values = (ref instanceof Array) ? ref : Object.keys(ref)
 
                     for (let value of values) layerValueSet.add(value)
 
-                } else if (layer < targetLayer && !(obj instanceof Array)) {
+                } else if (layer < targetLayer && !(ref instanceof Array)) {
 
                     let inputValue = eleInputs[layer].value
 
-                    let objs = inputValue.trim() ? [obj[inputValue]] : Object.values(obj)
+                    let refs = inputValue.trim() ? [ref[inputValue]] : Object.values(ref)
 
-                    for (let obj of objs) obj && walk(obj, layer + 1)
+                    for (let ref of refs) ref && walk(ref, layer + 1)
 
                 }
 
             }
 
-            walk(data)
+            walk(treeData)
 
             return layerValueSet
         }
