d1d0deb61b2c24c11d7e9efd6fac18a18bbafb51
Tim <timteam@qq.com>
2018-10-30 10:25:04 +0800

    Signed-off-by: Tim <timteam@qq.com>

---
+++ app/Http/Controllers/Frank/CtgController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 18.10.30
+ * Time: 10:05
+ */
+
+namespace App\Http\Controllers\Frank;
+
+
+use Illuminate\Http\Request;
+
+class CtgController extends Controller {
+
+    public function index(Request $req){
+
+    }
+
+}
---
+++ routes/frank.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Frank routes
+ *
+ * URI Prefix: None
+ * Controller Namespace: App\Http\Controllers\Frank
+ *
+ */
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/productguide', 'BrandLineController@index');
+
+
+
+
+
+
+
+
