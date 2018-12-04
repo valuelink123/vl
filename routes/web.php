e2f6214d7ed8ae56c29e6187b92d0825bfea6336
wangjianeng <284299346@qq.com>
2018-12-04 14:18:02 +0800

    exception

--- routes/web.php
+++ routes/web.php
@@ -75,4 +75,5 @@ Route::Post('/fees/getdeal', 'FeesController@getdeal')->name('getdeal');
 Route::Post('/fees/getcoupon', 'FeesController@getcoupon')->name('getcoupon');
 Route::Post('/fees/getservice', 'FeesController@getservice')->name('getservice');
 Route::resource('mcforder', 'McforderController');
+Route::resource('autoprice', 'AutopriceController');
 Route::Post('/mcforder/get', 'McforderController@get')->name('getMcforder');
