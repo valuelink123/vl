8b2ca2fa66e8c1b451868c04f424c95e0c95bf2c
wangjianeng <284299346@qq.com>
2019-04-03 17:51:49 +0800

    f

--- app/Http/Controllers/SkuController.php
+++ app/Http/Controllers/SkuController.php
@@ -105,7 +105,7 @@ left join fbm_stock as b on a.item_code =b.item_code
 				$datas_details[str_replace('.','',$di['site']).'-'.$di['asin'].'-'.$di['weeks']] = $di;
 			}
 			
-			$oa_datas = DB::connection('oa')->table('formtable_main_193_dt1')->whereRaw('('.implode(' or ',$sku_site_arr).')')->get();
+			$oa_datas = [];//DB::connection('oa')->table('formtable_main_193_dt1')->whereRaw('('.implode(' or ',$sku_site_arr).')')->get();
 			$oa_datas=json_decode(json_encode($oa_datas), true);
 			foreach($oa_datas as $od){
 				$oa_data[$od['zhand'].'-'.$od['SKU']] = $od;
