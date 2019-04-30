36affa675ba17c10bb823e0b7efb75885f479884
wangjianeng <284299346@qq.com>
2019-04-30 09:51:50 +0800

    f

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,11 +47,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+		
+		if(substr($data['email'],-18)!='@valuelinkcorp.com') die('email not allow register!');
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
+		
     }
 
     /**
@@ -62,6 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+		
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
