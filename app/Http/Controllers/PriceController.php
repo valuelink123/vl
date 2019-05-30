5e54886c0d7f249d45661302769aba35b7dbca66
wangjianeng <284299346@qq.com>
2019-05-30 16:07:15 +0800

    f

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Accounts;
 use App\User;
+use App\Asin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use PDO;
@@ -38,9 +39,10 @@ class PriceController extends Controller
 	
 	public function get(Request $request){
 		if(!Auth::user()->can(['price-model'])) die('Permission denied -- price-model');
+		
 		$data = $request->get('data');
 		$array = $priceGroup = $monthGroup = $normalGroup= [];
-		
+		$sku=$bg=$bu=$sap_seller_id='';//array_get($request->get('data'),'0.value');
 		foreach($data as $val){
 			if(substr($val['name'],0,11)=='price-group'){
 				$vm = explode('][',$val['name']);
@@ -54,7 +56,24 @@ class PriceController extends Controller
 			}else{
 				$normalGroup[$val['name']]=($val['name']=='I_MATNR' || $val['name']=='I_ZCPLX')?trim($val['value']):round($val['value'],2);
 			}	
+			if($val['name']=='I_MATNR') $sku = $val['value'];
 		}
+		
+		if (Auth::user()->seller_rules) {
+			$rules = explode("-",Auth::user()->seller_rules);
+			if(array_get($rules,0)!='*') $bg=array_get($rules,0);
+			if(array_get($rules,1)!='*') $bu=array_get($rules,1);
+		} elseif (Auth::user()->sap_seller_id) {
+			$sap_seller_id = Auth::user()->sap_seller_id;
+		} else {
+		
+		}
+		$exAsin=Asin::where('item_no',$sku);
+		if($bg) $exAsin=$exAsin->where('bg',$bg);
+		if($bu) $exAsin=$exAsin->where('bu',$bu);
+		if($sap_seller_id) $exAsin=$exAsin->where('sap_seller_id',$sap_seller_id);
+		$exAsin = $exAsin->first();
+		if(!$exAsin) die(json_encode(array('result'=>0,'message'=>'Permission denied or Sku Error')));
 		$appkey = 'site0001';
 		$appsecret= 'testsite0001';
 		$array['sku']=array_get($normalGroup,'I_MATNR');
