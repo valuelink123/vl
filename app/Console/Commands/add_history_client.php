36286a7e3ae89e731a26a2514625569446422f2d
Tim <timteam@qq.com>
2019-06-13 21:29:02 +0800

    修改add_client.php和add_history_client.php这两个批处理文件，主要修改部分为：date为来源数据添加的时间，然后新插入了created_at、updated_at为当前时间，processor默认为原先数据的processor，或者根据订单找到该ASin所负责的销售

--- app/Console/Commands/add_history_client.php
+++ app/Console/Commands/add_history_client.php
@@ -22,6 +22,10 @@ class HistoryClient extends Command
 	 */
 	protected $description = 'Command description';
 
+	protected $sap = '';
+
+	protected $num = 500;
+
 	/**
 	 * Create a new command instance.
 	 *
@@ -42,13 +46,15 @@ class HistoryClient extends Command
 	function handle()
 	{
 		set_time_limit(0);
+		$this->sap = new SapRfcRequest();
 		DB::connection()->enableQueryLog(); // 开启查询日志
 		Log::Info('Add History Client Start...');
 		$this->getCtgCrm();
 		$this->getNonCtg();
-		$this->getEmailCrm();
+		// $this->getEmailCrm();//注意：未做完(需求待确认)
 		$this->getCallCrm();
-
+		$this->getRsgCrm();
+		$this->getReviewCrm();
 	}
 
 	/*
@@ -57,16 +63,15 @@ class HistoryClient extends Command
 	 */
 	function getCtgCrm()
 	{
-		$where = $timeRange = '1 = 1';
+		$where = '1 = 1';
 		$config = getCtgCashbackBogoConfig();//得到配置信息
 		foreach($config as $condata){
 			//sql查询数据，得到不存在的邮箱和订单号的数据，然后插入到CRM模块的相关表中
-			$sql = "SELECT  t1.name,t1.email,t1.phone,t1.order_id as amazon_order_id,t3.County as country,t4.brands as brand 
+			$sql = "SELECT  t1.name,t1.email,t1.phone,t1.order_id as amazon_order_id,t1.created_at as date,t1.processor as processor,t3.CountryCode as country,t4.brands as brand 
 					FROM {$condata['table']} t1
 					LEFT JOIN (
-					  	SELECT MarketPlaceId,SellerId,AmazonOrderId,County
+					  	SELECT MarketPlaceId,SellerId,AmazonOrderId,CountryCode
 					  FROM ctg_order
-						WHERE $timeRange
 					  ) t3
 					  ON t3.AmazonOrderId = t1.order_id
 					LEFT JOIN (
@@ -74,7 +79,6 @@ class HistoryClient extends Command
 						FROM ctg_order_item t4_1
 						LEFT JOIN asin
 						  ON asin.site = t4_1.MarketPlaceSite AND asin.asin = t4_1.ASIN AND asin.sellersku = t4_1.SellerSKU
-						WHERE $timeRange
 						GROUP BY MarketPlaceId,AmazonOrderId,SellerId
 					  ) t4
 					  ON t4.AmazonOrderId = t1.order_id AND t4.MarketPlaceId = t3.MarketPlaceId AND t4.SellerId = t3.SellerId
@@ -84,14 +88,11 @@ class HistoryClient extends Command
 			Log::Info($sql);
 			$data = $this->queryRows($sql);
 			if($data){
-				$dataArr = array_chunk($data,200,true);
-				foreach($dataArr as $data) {
-					$this->addData($data, $condata['from']);
-				}
+				$this->addData($data, $condata['from'],false,false);
 			}
 		}
-		$queries = DB::getQueryLog(); // 获取查询日志
-		Log::Info($queries);
+		// $queries = DB::getQueryLog(); // 获取查询日志
+		// Log::Info($queries);
 	}
 
 	/*
@@ -100,9 +101,9 @@ class HistoryClient extends Command
 	 */
 	function getNonCtg()
 	{
-		$where = $timeRange = '1 = 1';
+		$where = '1 = 1';
 		//sql查询数据，得到不存在的邮箱和订单号的数据，然后插入到CRM模块的相关表中
-		$sql = "SELECT  t1.name,t1.email as email,t1.amazon_order_id as amazon_order_id,t3.brand as brand,'' as phone,'' as country  
+		$sql = "SELECT  t1.name,t1.email as email,t1.amazon_order_id as amazon_order_id,t1.processor as processor,t1.date as date,t3.brand as brand,'' as phone,'' as country  
 				FROM non_ctg as t1 
 				LEFT JOIN asin t3 ON t1.asin = t3.asin and t3.site = CONCAT('www.',t1.saleschannel) and t1.sellersku = t3.sellersku
 				left join client_info as t5 on t5.email = t1.email 
@@ -111,90 +112,127 @@ class HistoryClient extends Command
 		Log::Info($sql);
 		$data = $this->queryRows($sql);
 		if($data){
-			$dataArr = array_chunk($data,200,true);
-			foreach($dataArr as $data){
-				$data = $this->OrderInfoByData($data);
-				if($data){
-					$this->addData($data,'Non-CTG');
-				}
-			}
+			$this->addData($data,'Non-CTG',true,false);
 		}
 
-		$queries = DB::getQueryLog(); // 获取查询日志
-		Log::Info($queries);
+		// $queries = DB::getQueryLog(); // 获取查询日志
+		// Log::Info($queries);
 	}
 	/*
 	 * 得到crm模块来自email客户
 	 * 只获取site站点邮箱的客户
+	 * 来自email的数据的processor为最后一个发件人
+	 *
 	 */
-	function getEmailCrm()
-	{
-		$where = $timeRange = '1 = 1';
-		$sql = "SELECT  t1.from_name as name,t1.from_address as email,t1.amazon_order_id as amazon_order_id,t3.brand as brand,'' as phone,'' as country
-				FROM inbox as t1 
-				LEFT JOIN asin t3 ON t1.asin = t3.asin and t1.sku = t3.sellersku
-				left join client_info as t5 on t5.email = t1.from_address 
-				left join client_order_info as t6 on t6.amazon_order_id=t1.amazon_order_id 
-				WHERE {$where} and t1.from_address is not null and type = 'Site' and t6.id is null ";
-		Log::Info($sql);
-		$data = $this->queryRows($sql);
-		if($data){
-			$data = $this->OrderInfoByData($data);
-			$dataArr = array_chunk($data,200,true);
-			foreach($dataArr as $data) {
-				if ($data) {
-					$this->addData($data, 'Email');
-				}
-			}
-		}
-
-		$queries = DB::getQueryLog(); // 获取查询日志
-		Log::Info($queries);
-
-	}
+	// function getEmailCrm()
+	// {
+	// 	$where = '1 = 1';
+	// 	$sql = "SELECT  t1.from_name as name,t1.from_address as email,t1.amazon_order_id as amazon_order_id,t1.date as date,t3.brand as brand,'' as phone,'' as country
+	// 			FROM inbox as t1
+	// 			LEFT JOIN asin t3 ON t1.asin = t3.asin and t1.sku = t3.sellersku
+	// 			left join client_info as t5 on t5.email = t1.from_address
+	// 			left join client_order_info as t6 on t6.amazon_order_id=t1.amazon_order_id
+	// 			WHERE {$where} and t1.from_address is not null and type = 'Site' and t6.id is null limit 0,10";
+	// 	Log::Info($sql);
+	// 	$data = $this->queryRows($sql);
+	// 	if($data){
+	// 		$this->addData($data, 'Email',true,false);
+	// 	}
+	//
+	// 	// $queries = DB::getQueryLog(); // 获取查询日志
+	// 	// Log::Info($queries);
+	//
+	// }
 
 	/*
 	 * 得到crm模块来自Call客户
-	 *
+	 * 来自call的数据的processor为记录人(页面上的creator)
 	 */
 	function getCallCrm()
 	{
-		$where = $timeRange = '1 = 1';
-		$sql = "SELECT  '' as name,t1.buyer_email as email,t1.amazon_order_id as amazon_order_id,t3.brand as brand,'' as phone,'' as country 
+		$where = '1 = 1';
+		$sql = "SELECT  '' as name,t1.buyer_email as email,t1.amazon_order_id as amazon_order_id,t1.user_id as processor,t1.date as date,t3.brand as brand,'' as phone,'' as country 
 				FROM phone as t1 
-				LEFT JOIN asin t3 ON t1.asin = t3.asin and t1.sku = t3.sellersku
+				LEFT JOIN asin t3 ON t1.asin = t3.asin and t1.sku = t3.sellersku 
 				left join client_info as t5 on t5.email = t1.buyer_email 
 				left join client_order_info as t6 on t6.amazon_order_id=t1.amazon_order_id 
-				WHERE {$where} and t1.buyer_email is not null and t6.id is null ";
+				WHERE {$where} and t1.buyer_email is not null and t6.id is null";
 		Log::Info($sql);
 		$data = $this->queryRows($sql);
 		if($data){
-			$data = $this->OrderInfoByData($data);
-			if($data){
-				$this->addData($data,'Call');
-			}
+			$this->addData($data,'Call',true,false);
 		}
 
-		$queries = DB::getQueryLog(); // 获取查询日志
-		Log::Info($queries);
+		// $queries = DB::getQueryLog(); // 获取查询日志
+		// Log::Info($queries);
+	}
 
+	/*
+	 * 得到crm模块来自RSG客户
+	 * RSG，默认没有processor
+	 * 这个RSG的产品是这个销售的  但是客户不是他跟进的
+	 * brand,phone,country,processor需要通过sap接口获取订单的信息
+	 */
+	function getRsgCrm()
+	{
+		$where = '1 = 1';
+		$sql = "SELECT  '' as name,t1.customer_email as email,t1.amazon_order_id as amazon_order_id,t1.created_at as date,0 as processor,'' as brand,'' as phone,'' as country 
+				FROM rsg_requests as t1 
+				left join client_info as t5 on t5.email = t1.customer_email 
+				left join client_order_info as t6 on t6.amazon_order_id=t1.amazon_order_id 
+				WHERE {$where} and t1.customer_email is not null and t6.id is null";
+		Log::Info($sql);
+		$data = $this->queryRows($sql);
+		if($data){
+			$this->addData($data,'RSG',true,true);
+		}
+
+		// $queries = DB::getQueryLog(); // 获取查询日志
+		// Log::Info($queries);
+	}
+
+	/*
+	 * 得到crm模块来自review客户
+	 * processor为页面上的user
+	 */
+	function getReviewCrm()
+	{
+		$where = '1 = 1';
+		$sql = "SELECT  reviewer_name as name,t1.buyer_email as email,t1.amazon_order_id as amazon_order_id,t1.user_id as processor,t1.date as date,'' as brand,t1.buyer_phone as phone,'' as country 
+				FROM review as t1 
+				left join client_info as t5 on t5.email = t1.buyer_email 
+				left join client_order_info as t6 on t6.amazon_order_id=t1.amazon_order_id 
+				WHERE {$where} and t1.buyer_email is not null and t6.id is null";
+		Log::Info($sql);
+		$data = $this->queryRows($sql);
+		if($data){
+			$this->addData($data,'Review',true,false);
+		}
+
+		// $queries = DB::getQueryLog(); // 获取查询日志
+		// Log::Info($queries);
 	}
 
 	//插入数据到client,client_info,client_order_info这三个表中
-	function addData($_data,$from)
+	function addData($_data,$from,$sap=false,$processor=false)
 	{
 		DB::beginTransaction();
 		$insertOrder = array();
 		$data = array();
 		//处理数据，一个email下可能有好几个订单
 		foreach($_data as $key=>$val){
+			if($sap){
+				$val = $this->OrderInfoByData($val,$processor);
+			}
 			$order_id = $val['amazon_order_id'];
 			unset($val['amazon_order_id']);
 			if(!isset($data[$val['email']])){
 				$data[$val['email']] = $val;
 			}
+
 			$data[$val['email']]['amazon_order_id'][] = $order_id;
 		}
+
 		foreach($data as $key=>$val){
 			//检查是否有相同的邮箱,email要保持唯一性
 			$info = DB::table('client_info')->where('email',$val['email'])->get(array('id'))->toArray();
@@ -210,7 +248,16 @@ class HistoryClient extends Command
 					'brand'=>$val['brand'],
 					'from'=>$from,
 				);
-				$insertInfo['client_id'] = $res = DB::table('client')->insertGetId(array('date'=>date('Y-m-d')));
+				//插入到client表里的数据0
+				$userId = $val['processor'];
+				$insertClient = array(
+					'date'=>isset($val['date']) ? $val['date'] : date('Y-m-d H:i:s'),
+					'created_at'=>date('Y-m-d H:i:s'),
+					'updated_at'=>date('Y-m-d H:i:s'),
+					'processor' => $userId,
+				);
+
+				$insertInfo['client_id'] = $res = DB::table('client')->insertGetId($insertClient);
 				$ci_id = DB::table('client_info')->insertGetId($insertInfo);
 			}
 			if(empty($res) || empty($ci_id)){
@@ -219,10 +266,17 @@ class HistoryClient extends Command
 			}
 			foreach($val['amazon_order_id'] as $v){
 				if($v){
+					//RSG的订单要特殊标记
+					$order_type = 0;
+					if($from=='RSG'){
+						$order_type = 1;
+					}
 					$insertOrder[] = array(
 						'amazon_order_id' => $v,
 						'ci_id' => $ci_id,
+						'order_type' => $order_type,
 					);
+
 				}
 			}
 		}
@@ -236,25 +290,33 @@ class HistoryClient extends Command
 	/*
 	 * 从sap的获取订单信息接口得到表里面没有的数据，例如country，phone
 	 */
-	function OrderInfoByData($data)
+	function OrderInfoByData($data,$processor)
 	{
-		$sap = new SapRfcRequest();
-		foreach($data as $key=>$val){
-			$orderid = $val['amazon_order_id'];
-			$match = matchOrderId($orderid);
-			if($orderid){
-				if($match){
-					try {
-						$sapOrderInfo = SapRfcRequest::sapOrderDataTranslate($sap->getOrder(['orderId' => $orderid]));
-						$data[$key]['country'] = $val['country'] ? $val['country'] : $sapOrderInfo['CountryCode'];
-						$data[$key]['phone'] = $val['phone'] ? $val['phone'] : $sapOrderInfo['Phone'];
-						$data[$key]['name'] = $val['name'] ? $val['name'] : $sapOrderInfo['Name'];
-					} catch (\Exception $e) {
-						$data[$key]['amazon_order_id'] = '';
+		$sap = $this->sap;
+
+		$orderid = $data['amazon_order_id'];
+		$match = matchOrderId($orderid);
+		if($orderid){
+			if($match){
+				try {
+					$sapOrderInfo = SapRfcRequest::sapOrderDataTranslate($sap->getOrder(['orderId' => $orderid]));
+					$data['country'] = isset($data['country']) && $data['country'] ? $data['country'] : $sapOrderInfo['CountryCode'];
+					$data['phone'] = isset($data['phone']) && $data['phone'] ? $data['phone'] : $sapOrderInfo['Phone'];
+					$data['name'] = isset($data['name']) && $data['name'] ? $data['name'] : $sapOrderInfo['Name'];
+					$data['processor'] = isset($data['processor']) && $data['processor'] ? $data['processor'] : 0;
+					if($processor && isset($sapOrderInfo['orderItems'][0]['ASIN'])){
+						$sql = "select t2.id as user_id
+										from asin as t1
+										left join users as t2 on t2.sap_seller_id = t1.sap_seller_id
+										where asin = '{$sapOrderInfo['orderItems'][0]['ASIN']}' and site = 'www.{$sapOrderInfo['SalesChannel']}' and sellersku = '{$sapOrderInfo['orderItems'][0]['SellerSKU']}' limit 1";
+						$userData = $this->queryRows($sql);
+						$data['processor'] = isset($userData[0]['user_id']) ? $userData[0]['user_id'] : 0;
 					}
-				}else{
-					$data[$key]['amazon_order_id'] = '';
+				} catch (\Exception $e) {
+					$data['amazon_order_id'] = '';
 				}
+			}else{
+				$data['amazon_order_id'] = '';
 			}
 		}
 		return $data;
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -88,9 +88,9 @@ class Kernel extends ConsoleKernel
         // $schedule->command('add:nonctg')->monthly()->appendOutputTo($filePath)->name('add_history_nonctg')->withoutOverlapping();//添加nonctg数据，此脚本只执行一次
         $schedule->command('update:nonctg')->cron('*/30 * * * *')->appendOutputTo($filePath)->name('update_nonctg')->withoutOverlapping();
         //crm模块的相关批处理
-		// $schedule->command('add:historyClient')->everyMinute()->name('add_history_client')->withoutOverlapping();//添加客户数据，此脚本只执行一次
+		$schedule->command('add:historyClient')->monthly()->name('add_history_client')->withoutOverlapping();//添加客户数据，此脚本只执行一次
 		$schedule->command('sta:client')->dailyAt('1:00')->name('sta_client')->withoutOverlapping();//统计历史客户数据，每天跑一次
-		// $schedule->command('add:client')->dailyAt('2:00')->name('add_client')->withoutOverlapping();//添加客户数据，每天跑一次
+		$schedule->command('add:client')->dailyAt('2:00')->name('add_client')->withoutOverlapping();//添加客户数据，每天跑一次
 		$schedule->command('insert:asininfo')->dailyAt('3:00')->name('insertasin')->withoutOverlapping();
     }
 
