8c1d0c462349fea407ee0b6e81890a6ca71a0ce0
wangjianeng <284299346@qq.com>
2019-09-30 15:59:07 +0800

    f

--- app/Classes/SapRfcRequest.php
+++ app/Classes/SapRfcRequest.php
@@ -30,7 +30,7 @@ class SapRfcRequest {
     private $appid;
     private $appsecret;
 
-    public function __construct($appid = 'site0001', $appsecret = 'testsite0001', $host = env('SAP_RFC')) {
+    public function __construct($appid = 'site0001', $appsecret = 'testsite0001', $host = '113.108.40.136:18003') {
         $this->host = $host;
         $this->appid = $appid;
         $this->appsecret = $appsecret;
