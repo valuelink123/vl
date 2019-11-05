ed9c5c07afddc65aa7b48653984e7c5c15b78024
wangjianeng <284299346@qq.com>
2019-11-05 17:55:44 +0800

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -128,13 +128,13 @@ class HomeController extends Controller
 		$daily_info = SkusDailyInfo::select(DB::raw('sum(bonus)*'.$bonus_point.' as sumbonus,date'))->whereRaw($sumwhere." and date>='$date_from' and date<='$date_to'")->groupBy(['date'])->pluck('sumbonus','date');
 		
 		foreach($asins as $key=>$asin){
-			$sku_info = SkusDailyInfo::select(DB::raw('sum(bonus)*'.$bonus_point.' as bonus,sum(economic) as economic,sum(amount) as amount,sum(sales) as sales'))->whereRaw("sku='".$asin['item_no']."' and site='".$asin['site']."' and date>='$date_from' and date<='$date_to'")->first()->toArray();
+			$sku_info = SkusDailyInfo::select(DB::raw('sum(bonus)*'.$bonus_point.' as bonus,sum(economic) as economic'))->whereRaw("sku='".$asin['item_no']."' and site='".$asin['site']."' and date>='$date_from' and date<='$date_to'")->first()->toArray();
 			$asins[$key]['bonus']=$sku_info['bonus'];
 			$asins[$key]['economic']=$sku_info['economic'];
 			
 			$asin_info = AsinDailyInfo::select(DB::raw('sum(amount) as amount,sum(sales) as sales'))->whereRaw("asin='".$asin['asin']."' and site='".$asin['site']."' and date>='$date_from' and date<='$date_to'")->first()->toArray();
-			$asins[$key]['amount']=$sku_info['amount'];
-			$asins[$key]['sales']=$sku_info['sales'];
+			$asins[$key]['amount']=$asin_info['amount'];
+			$asins[$key]['sales']=$asin_info['sales'];
 		}
 	
 		$returnDate['bonus_point']= $bonus_point;
