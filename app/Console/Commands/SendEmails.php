42c7b59c3f142b33d7066c8cbb7a5909c5dc5c3b
wangjianeng <284299346@qq.com>
2019-11-06 11:33:56 +0800

    f

--- app/Console/Commands/SendEmails.php
+++ app/Console/Commands/SendEmails.php
@@ -50,7 +50,7 @@ class SendEmails extends Command
      */
     public function handle()
     {
-		set_time_limit(0);
+		set_time_limit(300);
         $count = $smtp_array = $signature_arrays = array();
         $command = $this;
         $smtp_config =  Accounts::whereNotNull('smtp_host')->whereNotNull('smtp_port')->whereNotNull('smtp_ssl')->get();
@@ -66,7 +66,7 @@ class SendEmails extends Command
 		
 		
 		
-        $tasks = Sendbox::where('status','Waiting')->where('plan_date','<',strtotime(date('Y-m-d H:i:s')))->where('error_count','<',1)->orderBy('from_address','asc')->take(500)->get();
+        $tasks = Sendbox::where('status','Waiting')->where('plan_date','<',strtotime(date('Y-m-d H:i:s')))->where('error_count','<',1)->orderBy('from_address','asc')->take(50)->get();
 		$this->run_email = '';
 		foreach ($tasks as $task) {
 
