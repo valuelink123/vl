03d77b0bf152b40c953d53cc224e54087e7bec95
Tim <timteam@qq.com>
2019-11-14 17:41:11 +0800

    修改黑色星期五和网络星期一的日期

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,11 +19,11 @@ class Controller extends BaseController
 
 	public function __construct()
 	{
-		//计算倒计时相关天数,Black Friday=>'11.22','Cyber Monday'=>'11.25','Christmas'=>'12.24'
+		//计算倒计时相关天数,Black Friday=>'11.29','Cyber Monday'=>'12.2','Christmas'=>'12.24'
 		$this->middleware(function ($request, $next) {
 			$year = date('Y');
-			$countDown['black_friday'] = (strtotime(date($year.'-11-22'))-strtotime(date('Y-m-d')))/86400;
-			$countDown['cyber_monday'] = (strtotime(date($year.'-11-25'))-strtotime(date('Y-m-d')))/86400;
+			$countDown['black_friday'] = (strtotime(date($year.'-11-29'))-strtotime(date('Y-m-d')))/86400;
+			$countDown['cyber_monday'] = (strtotime(date($year.'-12-2'))-strtotime(date('Y-m-d')))/86400;
 			$countDown['christmas'] = (strtotime(date($year.'-12-24'))-strtotime(date('Y-m-d')))/86400;
 			session()->put('countDown',$countDown);
 			return $next($request);
