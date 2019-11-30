da73755afcd510aafdb8605d9bc78d1161e25228
wangjianeng <284299346@qq.com>
2019-11-30 13:48:57 +0800

    f

--- app/Console/Commands/GetEmails.php
+++ app/Console/Commands/GetEmails.php
@@ -105,7 +105,7 @@ class GetEmails extends Command
 			$messages = $mailbox->getMessages($search,\SORTDATE,false);
 			$auto_exit=0;
 			$auto_exit_num=0;
-			if(emtpy($messages)){
+			if(empty($messages)){
 				$messages = $mailbox->getMessages(
 					NULL,
 					\SORTDATE, // Sort criteria
