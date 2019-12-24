e63cf229ea2c84a2e08d2c14bbf833be6a229f9f
wangjianeng <284299346@qq.com>
2019-12-24 09:28:05 +0800

    f

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -34,7 +34,7 @@ class BudgetController extends Controller
 	
     public function index(Request $request)
     {	
-		//if(!Auth::user()->can(['budgets-show'])) die('Permission denied -- budgets-show');
+		if(!Auth::user()->can(['budgets-show'])) die('Permission denied -- budgets-show');
 		if($request->isMethod('POST')){ 
 
 			Budgets::whereIn('id',$request->get('budget_id'))->update(['status'=>$request->get('BatchUpdate')]);
