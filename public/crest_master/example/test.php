80945daa75449c5196334ca9cec23a985ea949cf
duanyongsheng@dtas.com <duanyongsheng@dtas.com>
2020-05-19 16:08:02 +0800

    demo

--- public/crest_master/example/test.php
+++ public/crest_master/example/test.php
@@ -1,16 +1,22 @@
 <?php
 include_once ('../src/crest.php');
-$result = CRest::call(
-    'crm.lead.add',
+//$result = CRest::call(
+//    'crm.lead.add',
+//    [
+//        'fields'=>[
+//            'TITLE'=>'title123',
+//            'NAME'=>'mingzi',
+//            'PHONE'=>'123456',
+//            'EMAIL'=>'235235@qq.com',
+//            'SECOND_NAME'=>'name1241'
+//        ]
+//
+//    ]
+//);
+$result1 = CRest::call(
+    'crm.lead.get',
     [
-        'fields'=>[
-            'TITLE'=>'title123',
-            'NAME'=>'mingzi',
-            'PHONE'=>'123456',
-            'EMAIL'=>'235235@qq.com',
-            'SECOND_NAME'=>'name1241'
-        ]
-
+        'id'=>44595,
     ]
 );
 var_dump($result);
