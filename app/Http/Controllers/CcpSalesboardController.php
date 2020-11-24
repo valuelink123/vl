bea7ebf4666b49ea6c5ea031878015185eb7c8f9
Wulanfang <wulanfang@valuelinkltd.com>
2020-11-24 15:36:27 +0800

    ccp环比数据分析功能中，为查询的时间加上夏令制时间和冬令制时间的范围的不同

--- app/Http/Controllers/CcpSalesboardController.php
+++ app/Http/Controllers/CcpSalesboardController.php
@@ -361,7 +361,7 @@ class CcpSalesBoardController extends Controller
 	    if(!$date_type) $date_type = 1;
         //如果选的时间类型是后台当地时间，时间要做转化
 		$configDays = array(1=>1,2=>1,3=>3,4=>7,5=>15,6=>30);
-		$time = $this->getPresentTime($site,$timeType);//获取当前时间戳
+		$time = $this->getCurrentTime($site,$timeType);//获取当前时间戳
         $startDate = date('Y-m-d 00:00:00',$time);//默认的开始时间
         $endDate = date('Y-m-d H:i:s',$time);//默认的结束时间
 		$dateRange = array();
@@ -389,28 +389,6 @@ class CcpSalesBoardController extends Controller
         }
 		return $dateRange;
 	}
-
-    //得到当前时间戳
-	public function getPresentTime($site,$timeType)
-	{
-		//如果选的时间类型是后台当地时间，时间要做转化
-		$dateconfig = array('A1PA6795UKMFR9','A1RKKUPIHCS9HS','A13V1IB3VIYZZH','APJ6JRA9NG5V4');//utc+2:00
-		$time = time();//北京时间当前时间戳
-		// $time = strtotime('2020-09-01');//测试日期
-		if($timeType==1){//选的是后台当地时间
-			if($site=='A1VC38T7YXB528'){//时间范围+1小时,日本站点,-8+9
-				$time = strtotime(date('Y-m-d H:i:s', strtotime ("+1 hour", $time)));//日本站后台当前时间;
-			}elseif($site=='A1F83G8C2ARO7P'){//英国站点+1小时，uTc+1:00,-8+1
-				$time = strtotime(date('Y-m-d H:i:s', strtotime ("-7 hour", $time)));//英国站后台当前时间;
-			}elseif(in_array($site,$dateconfig)){//utc+2:00,-8+2
-				$time = strtotime(date('Y-m-d H:i:s', strtotime ("-6 hour", $time)));//几个特殊的站点
-			}else{//时间范围-15小时,-8-7
-				$time =  strtotime(date('Y-m-d H:i:s', strtotime ("-15 hour", $time)));//美国站后台当前时间;
-			}
-		}
-		return $time;
-	}
-
 
     public function showAccountBySite(){
 	    return $this->showTheAccountBySite();
