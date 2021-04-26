325c63af44533d53905a8a762baa10607c7fbb92
wangjianeng <284299346@qq.com>
2021-04-26 14:37:03 +0800

    dailyreport

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -131,7 +131,7 @@ class Kernel extends ConsoleKernel
 		$schedule->command('add:sales_remind')->dailyAt('07:15')->name('addSalesRemind')->withoutOverlapping();//22周销售计划中，还没有填写销售计划的时候，插件提醒销售去添加计划
 		$schedule->command('add:asin_data')->dailyAt('07:30')->name('addAsinData')->withoutOverlapping();//添加asin数据，每日更新，每周库存跟在途等数据
 
-		$schedule->command('push:dailyReport --marketplace_id=ATVPDKIKX0DER --with_stock=1')->twiceDaily(14, 16);->name('dailyReportATVPDKIKX0DER')->withoutOverlapping();
+		$schedule->command('push:dailyReport --marketplace_id=ATVPDKIKX0DER --with_stock=1')->twiceDaily(14, 16)->name('dailyReportATVPDKIKX0DER')->withoutOverlapping();
 		$schedule->command('push:dailyReport --marketplace_id=A2EUQ1WTGCTBG2 --with_stock=1')->twiceDaily(14, 16)->name('dailyReportA2EUQ1WTGCTBG2')->withoutOverlapping();
 		$schedule->command('push:dailyReport --marketplace_id=A1AM78C64UM0Y8 --with_stock=1')->twiceDaily(14, 16)->name('dailyReportA1AM78C64UM0Y8')->withoutOverlapping();
 		$schedule->command('push:dailyReport --marketplace_id=A1F83G8C2ARO7P --with_stock=1')->twiceDaily(9,11)->name('dailyReportA1F83G8C2ARO7P')->withoutOverlapping();
