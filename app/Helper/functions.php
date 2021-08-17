cf23d7da12a812174323a3160510d586cc8ee513
Wulanfang <wulanfang@valuelinkltd.com>
2021-08-17 17:34:33 +0800

    异常单编辑页的时候，gift card中间几个字符串变成*号

--- app/Helper/functions.php
+++ app/Helper/functions.php
@@ -1752,3 +1752,34 @@ function getSellerAccout(){
 	}
 	return $data_account;
 }
+
+function func_substr_replace($str, $replacement = "*", $start = 3, $length = 6)
+{
+	$len = mb_strlen($str,"utf-8");
+
+	if ($len > intval($start+$length)) {
+		$str1 = mb_substr($str,0,$start,"utf-8");
+
+		$str2 = mb_substr($str,intval($start+$length),NULL,"utf-8");
+
+	} else {
+		$str1 = mb_substr($str,0,1,"utf-8");
+
+		$str2 = mb_substr($str,$len-1,1,"utf-8");
+
+		$length = $len - 2;
+
+	}
+
+	$new_str = $str1;
+
+	for ($i = 0; $i < $length; $i++) {
+		$new_str .= $replacement;
+
+	}
+
+	$new_str .= $str2;
+
+	return $new_str;
+
+}
