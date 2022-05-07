950284eb4bff4b5bf689b2ad96781fd730e7267a
creez <chenguancan@valuelinkltd.com>
2022-05-07 09:42:14 +0800

    增加wms数据库配置

--- config/database.php
+++ config/database.php
@@ -396,6 +396,21 @@ return [
 			'engine' => null,
 		],
 
+        'wms' => [
+            'driver' => 'mysql',
+            'host' => env('DB_WMS_CONNECTION', '127.0.0.1'),
+            'port' => env('DB_WMS_PORT', '3306'),
+            'database' => env('DB_WMS_DATABASE', 'forge'),
+            'username' => env('DB_WMS_USERNAME', 'forge'),
+            'password' => env('DB_WMS_PASSWORD', ''),
+            'unix_socket' => env('DB_WMS_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
 
     ],
 
