d34d45dfc804e99aa5860c5ed99bbc0de1823ec2
wangjianeng <wangjianeng@valuelinkltd.com>
2022-06-02 18:31:09 +0800

    重发权限

--- app/Http/Controllers/ExceptionController.php
+++ app/Http/Controllers/ExceptionController.php
@@ -33,7 +33,7 @@ class ExceptionController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['resendApi']);
 		parent::__construct();
     }
 
