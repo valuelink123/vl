bd6fafa4d4d52554a3de355ae4fcc1a9aa413fd4
wangjianeng <wangjianeng@valuelinkltd.com>
2022-10-28 13:44:02 +0800

    reim report

---
+++ app/Http/Controllers/ReimController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\User;
+use App\Models\AmazonShipmentItem;
+use Illuminate\Support\Facades\Auth;
+use PDO;
+use DB;
+use Illuminate\Http\Response;
+class ReimController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     *
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        parent::__construct();
+    }
+
+    public function index()
+    {
+		$accounts = DB::connection('amazon')->table('seller_accounts')->where('primary',1)->whereNull('deleted_at')->pluck('label','mws_seller_id');
+        return view('reim/list',['accounts'=>$accounts, 'users'=>getUsers('sap_seller')]);
+
+    }
+
+    public function get(Request $request)
+    {
+        $datas = AmazonShipmentItem::selectRaw('amazon_shipment_items.*, seller_accounts.label,asin.bg,asin.bu,asin.sap_seller_id,asin.sku,amazon_reimbursements.case_id as r_case_id,amazon_reimbursements.currency_unit,amazon_reimbursements.amount_total,amazon_reimbursements.quantity_reimbursed_inventory,amazon_shipments.shipment_status')
+		->leftJoin('amazon_shipments',function($q){
+            $q->on('amazon_shipment_items.seller_id', '=', 'amazon_shipments.seller_id')->on(			'amazon_shipment_items.shipment_id', '=', 'amazon_shipments.shipment_id');
+        })
+		->leftJoin('seller_accounts',function($q){
+            $q->on('amazon_shipment_items.seller_id', '=', 'seller_accounts.mws_seller_id');
+        })
+        ->leftJoin('amazon_reimbursements',function($q){
+            $q->on('amazon_shipment_items.case_id', '=', 'amazon_reimbursements.case_id');
+        })
+        ->leftJoin(DB::raw("(select seller_sku,any_value(sku) as sku,any_value(sap_seller_id) as sap_seller_id,any_value(sap_seller_bg) as bg,any_value(sap_seller_bu) as bu from sap_asin_match_sku group by seller_sku) as asin"),function($q){
+            $q->on('amazon_shipment_items.seller_sku', '=', 'asin.seller_sku');
+        })->where('seller_accounts.primary',1)->where('amazon_shipments.shipment_status','CLOSED')->whereRaw('amazon_shipment_items.quantity_shipped>amazon_shipment_items.quantity_received');
+        
+
+		if(array_get($_REQUEST,'seller_account_id')){
+            $datas = $datas->whereIn('amazon_shipment_items.seller_id',array_get($_REQUEST,'seller_account_id'));
+        }
+        if(array_get($_REQUEST,'sap_seller_id')){
+            $datas = $datas->whereIn('asin.sap_seller_id',array_get($_REQUEST,'sap_seller_id'));
+        }
+        if(array_get($_REQUEST,'bg')){
+            $datas = $datas->whereIn('asin.bg',array_get($_REQUEST,'bg'));
+        }
+        if(array_get($_REQUEST,'bu')){
+            $datas = $datas->whereIn('asin.bu',array_get($_REQUEST,'bu'));
+        }
+        if(array_get($_REQUEST,'date_from')){
+            $datas = $datas->where('amazon_shipment_items.updated_at','>=',array_get($_REQUEST,'date_from'));
+        }
+        if(array_get($_REQUEST,'date_to')){
+            $datas = $datas->where('amazon_shipment_items.updated_at','<=',array_get($_REQUEST,'date_to'));
+        }
+		if(array_get($_REQUEST,'shipment_id')){
+            $datas = $datas->where('amazon_shipment_items.shipment_id',array_get($_REQUEST,'shipment_id'));
+        }
+		
+		if(array_get($_REQUEST,'case_id')){
+            $datas = $datas->where('amazon_shipment_items.case_id',array_get($_REQUEST,'case_id'));
+        }
+        if(array_get($_REQUEST,'sku')){
+            $datas = $datas->whereIn('asin.sku',explode(',',str_replace([' ','	'],'',array_get($_REQUEST,'sku'))));
+        } 
+		
+		if(array_get($_REQUEST,'step')){
+            $datas = $datas->whereIn('amazon_shipment_items.step',array_get($_REQUEST,'step'));
+        }
+		
+        if(array_get($_REQUEST,'status')=='Pending'){
+            $datas = $datas->whereNotNull('amazon_shipment_items.case_id')->whereNull('amazon_reimbursements.case_id');
+        }
+		
+		if(array_get($_REQUEST,'status')=='Success'){
+            $datas = $datas->whereNotNull('amazon_shipment_items.case_id')->whereNotNull('amazon_reimbursements.case_id');
+        }
+        
+        $iTotalRecords = $datas->count();
+        $iDisplayLength = intval($_REQUEST['length']);
+        $iDisplayLength = $iDisplayLength < 0 ? $iTotalRecords : $iDisplayLength;
+        $iDisplayStart = intval($_REQUEST['start']);
+        $sEcho = intval($_REQUEST['draw']);
+        $lists =  $datas->offset($iDisplayStart)->limit($iDisplayLength)->orderBy('amazon_shipment_items.id','desc')->get()->toArray();
+        $users = getUsers('sap_seller');
+        $records["data"] = array();
+		foreach ( $lists as $list){
+            $records["data"][] = array(
+                '<input name="id[]" type="checkbox" class="checkboxes" value="'.$list['id'].'"  />',
+                $list['updated_at'],
+                $list['bg'].'-'.$list['bu'],
+                array_get($users,$list['sap_seller_id'],$list['sap_seller_id']),
+                $list['label'],
+                $list['shipment_id'],
+				$list['seller_sku'],
+                $list['sku'],
+				$list['quantity_shipped'],
+				$list['quantity_received'],
+				$list['quantity_shipped']-$list['quantity_received'],
+				$list['shipment_status'],
+				'<a href="'.$list['pod'].'" target="_blank">'.basename($list['pod']).'</a>',
+				$list['case_id'],
+                $list['amount_total'],
+                $list['currency_unit'],
+                $list['quantity_reimbursed_inventory'],
+                empty($list['r_case_id'])?(empty($list['case_id'])?'':'Pending'):'Success',
+				array_get(AmazonShipmentItem::STATUS,$list['step'],$list['step']),
+				$list['remark'],
+            );
+		}
+
+        $records["draw"] = $sEcho;
+        $records["recordsTotal"] = $iTotalRecords;
+        $records["recordsFiltered"] = $iTotalRecords;
+        echo json_encode($records);
+    }
+	
+	public function edit(Request $request,$id)
+    {
+        return view('reim/edit',['id'=>$id]);
+    }
+
+    public function batchUpdate(Request $request){
+        DB::beginTransaction();
+        try{ 
+            $customActionMessage='';
+			$file = $request->file('file');
+			$updateData=[];
+			if ($file) {
+				if ($file->isValid()) {
+					$originalName = $file->getClientOriginalName();
+					$ext = $file->getClientOriginalExtension();
+					$type = $file->getClientMimeType();
+					$realPath = $file->getRealPath();
+					$newname = date('Y-m-d-H-i-S') . '-' . uniqid() . '.' . $ext;
+					$newpath = '/uploads/exceptionUpload/' . date('Ymd') . '/';
+					$inputFileName = public_path() . $newpath . $newname;
+					$bool = $file->move(public_path() . $newpath, $newname);
+					$updateData['pod']=$newpath . $newname;
+				}
+			}
+			$updateData['step']=intval($request->get('step'));
+			if($request->get('case_id')) $updateData['case_id']=$request->get('case_id');
+			if($request->get('remark')) $updateData['remark']=$request->get('remark');
+			$ids = explode(',',$request->get('ids'));
+            AmazonShipmentItem::whereIn('id',$ids)->update($updateData);
+            DB::commit();
+            $records["customActionStatus"] = 'OK';
+            $records["customActionMessage"] = $customActionMessage;     
+        }catch (\Exception $e) { 
+            DB::rollBack();
+            $records["customActionStatus"] = '';
+            $records["customActionMessage"] = $e->getMessage();
+        }    
+        echo json_encode($records);   
+
+    }
+
+}
---
+++ resources/views/reim/list.blade.php
@@ -0,0 +1,284 @@
+@extends('layouts.layout')
+@section('label', '索赔')
+@section('content')
+<style type="text/css">
+	th, td { white-space: nowrap;word-break:break-all; }
+</style>
+    <div class="row">
+        <div class="col-md-12">
+            <!-- BEGIN EXAMPLE TABLE PORTLET-->
+            <div class="portlet light bordered">
+                <div class="table-toolbar">
+                    <form role="form" action="{{url('reims')}}" method="GET">
+                        {{ csrf_field() }}
+                        <div class="row">
+						<div class="col-md-2">
+                            <div class="input-group date date-picker" data-date-format="yyyy-mm-dd">
+                                <input type="text" class="form-control" readonly name="date_from" placeholder="Date From" value="{{date('Y-m-d',strtotime('-30 days'))}}">
+                                <span class="input-group-btn">
+                                                                        <button class="btn btn-sm default" type="button">
+                                                                            <i class="fa fa-calendar"></i>
+                                                                        </button>
+                                                                    </span>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="input-group date date-picker" data-date-format="yyyy-mm-dd">
+                                <input type="text" class="form-control " name="date_to" placeholder="Date To" value="{{date('Y-m-d')}}">
+                                <span class="input-group-btn">
+                                                                        <button class="btn btn-sm default" type="button">
+                                                                            <i class="fa fa-calendar"></i>
+                                                                        </button>
+                                                                    </span>
+                            </div>
+                        </div>
+						
+						<div class="col-md-2">
+						<select class="mt-multiselect btn btn-default " multiple="multiple" data-label="left" data-width="100%" data-filter="true" data-action-onchange="true" data-none-selected-text="选择账号" name="seller_account_id[]" id="seller_account_id[]">
+                            @foreach ($accounts as $k=>$v)
+                                <option value="{{$k}}">{{$v}}</option>
+                            @endforeach
+                        </select>
+						</div>
+						<div class="col-md-2">
+						<select class="mt-multiselect btn btn-default " multiple="multiple" data-label="left" data-width="100%" data-filter="true" data-action-onchange="true" data-none-selected-text="选择销售" name="sap_seller_id[]" id="sap_seller_id[]">
+                            @foreach ($users as $k=>$v)
+                                <option value="{{$k}}">{{$v}}</option>
+                            @endforeach
+                        </select>
+						</div>
+                        <div class="col-md-2">
+						<select class="mt-multiselect btn btn-default " multiple="multiple" data-label="left" data-width="100%" data-filter="true" data-action-onchange="true" data-none-selected-text="选择BG" name="bg[]" id="bg[]">
+                            @foreach (getUsers('sap_bg') as $k=>$v)
+                                <option value="{{$v->bg}}">{{$v->bg}}</option>
+                            @endforeach
+                        </select>
+						</div>
+                        <div class="col-md-2">
+						<select class="mt-multiselect btn btn-default " multiple="multiple" data-label="left" data-width="100%" data-filter="true" data-action-onchange="true" data-none-selected-text="选择BU" name="bu[]" id="bu[]">
+                            @foreach (getUsers('sap_bu') as $k=>$v)
+                                <option value="{{$v->bu}}">{{$v->bu}}</option>
+                            @endforeach
+                        </select>
+						</div>
+                        <div class="col-md-2">
+						<input type="text" class="form-control " name="shipment_id" placeholder="Shipmend ID">
+						</div>
+						
+						<div class="col-md-2">
+						<input type="text" class="form-control " name="case_id" placeholder="CASE ID">
+						</div>
+						
+						<div class="col-md-2">
+						<input type="text" class="form-control" name="sku" placeholder="Sku">
+						</div>
+						<div class="col-md-2">
+						<select class="mt-multiselect btn btn-default " multiple="multiple" data-label="left" data-width="100%" data-filter="true" data-action-onchange="true" data-none-selected-text="选择进度" name="step[]" id="step[]">
+                            @foreach (\App\Models\AmazonShipmentItem::STATUS as $k=>$v)
+                                <option value="{{$k}}">{{$v}}</option>
+                            @endforeach
+                        </select>
+						</div>
+						<div class="col-md-2">
+						<select class="form-control " name="status" id="status" >
+                            <option value="" >All</option>
+							<option value="Pending" >Pending</option>
+							<option value="Success" >Success</option>
+                        </select>
+						</div>
+						
+						<div class="col-md-2">
+							
+							<button type="button" class="btn blue" id="data_search">搜索</button>
+									
+                            </div>
+
+						</div>	
+						
+
+                    </form>
+					
+					
+					
+					
+					
+					
+                </div>
+				
+                <div class="portlet-title">
+                    <div class="caption font-dark">
+                        <i class="icon-settings font-dark"></i>
+                        <span class="caption-subject bold uppercase">索赔列表</span>
+                    </div>
+					<div class="btn-group " style="float:right;">
+                        <div class="table-actions-wrapper" id="table-actions-wrapper">
+                            <button class="btn  green table-status-action-submit">
+                                <i class="fa fa-check"></i> 执行批量更新
+                            </button>
+                        </div>
+                    </div>
+                </div>
+				
+                <div class="portlet-body">
+                    <div class="table-container">
+                        <table class="table table-striped table-bordered table-hover table-checkable" id="datatable_ajax">
+                            <thead>
+                                <tr role="row" class="heading">
+                                    <th>
+                                        <input type="checkbox" class="group-checkable" data-set="#datatable_ajax .checkboxes" />
+                                    </th>
+                                    <th>更新日期</th>
+									<th>Bg - Bu</th>
+									<th>销售员</th>
+									<th>账号</th>
+									<th>ShipmentID</th>
+									<th>SellerSku</th>
+									<th>Sku</th>
+									<th>发货数量</th>
+									<th>收货数量</th>
+									<th>差异数量</th>
+									<th>货件状态</th>
+                                    <th>Pod</th>
+									<th>CaseID</th>
+									<th>赔偿金额</th>
+									<th>货币</th>
+                                    <th>赔偿数量</th>
+                                    <th>索赔状态</th>
+									<th>进度</th>
+                                    <th>备注</th>                      
+                                </tr>
+                            </thead>
+                            <tbody>	
+                            </tbody>
+                        </table>
+					</div>
+                </div>
+            </div>
+            <!-- END EXAMPLE TABLE PORTLET-->
+        </div>
+    </div>
+
+
+    <script>
+        var TableDatatablesAjax = function () {
+
+        var initPickers = function () {
+            //init date pickers
+            $('.date-picker').datepicker({
+                rtl: App.isRTL(),
+                autoclose: true
+            });
+        }
+
+        var initTable = function () {
+            $.ajaxSetup({
+                headers: { 'X-CSRF-TOKEN' : '{{ csrf_token() }}' }
+            });
+            var grid = new Datatable();
+            grid.setAjaxParam("date_from", $("input[name='date_from']").val());
+			grid.setAjaxParam("date_to", $("input[name='date_to']").val());
+			grid.setAjaxParam("shipment_id", $("input[name='shipment_id']").val());
+			grid.setAjaxParam("case_id", $("input[name='case_id']").val());
+			grid.setAjaxParam("sku", $("input[name='sku']").val());
+			grid.setAjaxParam("sap_seller_id", $("select[name='sap_seller_id[]']").val());
+            grid.setAjaxParam("seller_account_id", $("select[name='seller_account_id[]']").val());
+			grid.setAjaxParam("bg", $("select[name='bg[]']").val());
+            grid.setAjaxParam("bu", $("select[name='bu[]']").val());
+			grid.setAjaxParam("step", $("select[name='step[]']").val());
+            grid.setAjaxParam("status", $("select[name='status']").val());
+            grid.init({
+                src: $("#datatable_ajax"),
+                onSuccess: function (grid, response) {
+                    grid.setAjaxParam("customActionType", '');
+                },
+                onError: function (grid) {
+                },
+                onDataLoad: function(grid) {
+                },
+                loadingMessage: 'Loading...',
+                dataTable: {
+                   //"serverSide":false,
+                   "autoWidth":false,
+                   "ordering": false,
+                    "lengthMenu": [
+                        [10, 20, 50, -1],
+                        [10, 20, 50, 'All'] 
+                    ],
+                    "pageLength": 10,
+                    "ajax": {
+                        "url": "{{ url('reims/get')}}",
+                    },
+                    //"scrollX": true,
+                    //"autoWidth":true
+                    /*
+                    dom: 'Bfrtip',
+                    buttons: [ 
+                        {
+                            extend: 'excelHtml5',
+                            text: '导出当前页',
+                            title: 'Data export',
+                            exportOptions: {
+                                columns: [ 3,2,6,7,8,9,4,5 ]
+                            }
+                        },
+                     ]
+                     */
+                 }
+            });
+
+            //批量更改状态操作
+            $(".btn-group").unbind("click").on('click', '.table-status-action-submit', function (e) {
+                e.preventDefault();
+
+                if (grid.getSelectedRowsCount() > 0) {
+					$('#ajax').modal({
+						remote: '/reims/'+grid.getSelectedRows()+'/edit',
+					});
+                }else if (grid.getSelectedRowsCount() === 0) {
+                    toastr.error('No record selected');
+                }
+            });
+
+        }
+
+
+        return {
+
+            //main function to initiate the module
+            init: function () {
+                initPickers();
+                initTable();
+            }
+
+        };
+
+    }();
+
+$(function() {
+
+	TableDatatablesAjax.init();
+	$('#data_search').on('click',function(){
+		var dttable = $('#datatable_ajax').dataTable();
+		dttable.fnClearTable(false);
+	    dttable.fnDestroy(); 
+		TableDatatablesAjax.init();
+	});
+	$('#ajax').on('hidden.bs.modal', function (e) {
+        $('#ajax .modal-content').html('<div class="modal-body" >Loading...</div>');
+    });
+});
+
+
+</script>
+
+<div class="modal fade bs-modal-lg" id="ajax" role="basic" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content" >
+			<div class="modal-body" >
+				Loading...
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+
